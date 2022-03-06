86e21e522e62baf81d0bcb28fed5816fa4927857
Cavoi Rom <cavoirom.wanted@yahoo.com>
2022-03-06 15:06:32 +0700

    Added keymap for Mint Keyboard v1.1. The keymap only support Dvorak layout and custom key specific for propramming and MacOS.

---
+++ keyboards/ngxv/mint/readme.md
@@ -0,0 +1,6 @@
+# Mint
+
+47 keys ortholinear keyboard.
+
+* Keyboard Maintainer: [Cavoi Rom](https://github.com/cavoirom)
+* Hardware Supported: Weact STM32F411 Black Pill
---
+++ keyboards/ngxv/mint/rules.mk
@@ -0,0 +1,17 @@
+# MCU name
+MCU = STM32F411
+
+# Address of the bootloader in system memory
+STM32_BOOTLOADER_ADDRESS = 0x1FFF0000
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # USB Nkey Rollover, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+AUDIO_ENABLE = no           # Audio output
