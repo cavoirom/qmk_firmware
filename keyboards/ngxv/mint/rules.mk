aa53fc0f8d392a969c6f9237122c888816cfe17e
Cavoi Rom <cavoirom.wanted@yahoo.com>
2022-09-04 22:51:29 +0700

    Update symbol layout for better coding.

--- keyboards/ngxv/mint/rules.mk
+++ keyboards/ngxv/mint/rules.mk
@@ -1,6 +1,10 @@
 # MCU name
 MCU = STM32F411
 
+# Bootloader selection
+# See: https://github.com/qmk/qmk_firmware/blob/master/docs/flashing.md
+BOOTLOADER = stm32-dfu
+
 # Address of the bootloader in system memory
 STM32_BOOTLOADER_ADDRESS = 0x1FFF0000
 
