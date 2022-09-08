35548b9463035c81e36d3e8ec36d67654a614ab9
Cavoi Rom <cavoirom.wanted@yahoo.com>
2022-09-08 11:10:20 +0700

    Reduce typing latency.

--- keyboards/ngxv/mint/rules.mk
+++ keyboards/ngxv/mint/rules.mk
@@ -12,11 +12,13 @@ STM32_BOOTLOADER_ADDRESS = 0x1FFF0000
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes           # USB Nkey Rollover, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-AUDIO_ENABLE = no           # Audio output
+BOOTMAGIC_ENABLE = yes        # Enable Bootmagic Lite.
+MOUSEKEY_ENABLE = no          # Mouse keys.
+EXTRAKEY_ENABLE = yes         # Audio control and System control.
+CONSOLE_ENABLE = no           # Console for debug.
+COMMAND_ENABLE = no           # Commands for debug and configuration.
+NKRO_ENABLE = yes             # USB Nkey Rollover, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+BACKLIGHT_ENABLE = no         # Enable keyboard backlight functionality.
+AUDIO_ENABLE = no             # Audio output.
+DEBOUNCE_TYPE = sym_defer_g   # Default debounce algorithm, can debounce and denoise, suitable for metal plate.
+
